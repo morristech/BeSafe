1f12d76c1ac3fe39e6285ac5441d3900aa80cbd7
Felony <onyfel@gmail.com>
2018-04-30 13:49:31 +0430

    - Add ProcessWatcher class which raise given event on new process creation
    - Use "ExceptionManager" class helper to log exceptions to log file

---
+++ ClientService/Watchers/ProcessWatcher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Management;
+using ExceptionManager;
+
+namespace BeSafe.Watchers
+{
+    public class ProcessWatcher
+    {
+        readonly ManagementEventWatcher _processStartEvent = new ManagementEventWatcher("SELECT * FROM Win32_ProcessStartTrace");
+
+        public ProcessWatcher(EventArrivedEventHandler onProcessStartEventHandler)
+        {
+            try
+            {
+                _processStartEvent.EventArrived += onProcessStartEventHandler;
+                _processStartEvent.Start();
+            }
+            catch (Exception ex)
+            {
+                ex.LogToFile(ExceptionType.Service);
+            }
+        }
+
+        public bool Stop()
+        {
+            try
+            {
+                _processStartEvent.Stop();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.LogToFile(ExceptionType.Service);
+                return false;
+            }
+        }
+    }
+}
