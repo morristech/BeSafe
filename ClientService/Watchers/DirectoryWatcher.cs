250cd69dfa29b2e06e60775c04a28cdae714533c
MojtabaTajik <onyfel@gmail.com>
2018-05-03 11:00:46 +0430

    - Add DirectoryWatcher class which raise an event on file change occured - Add FileInfo class which store information about changed file

---
+++ ClientService/Watchers/DirectoryWatcher.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using ExceptionManager;
+
+namespace BeSafe.Watchers
+{
+    public class DirectoryWatcher
+    {
+        public delegate void FileChangeEventHandler(Types.FileInfo fileInfo);
+        public FileChangeEventHandler OnFileChange;
+
+        private readonly FileSystemWatcher directoryChangeWatcher;
+
+        public DirectoryWatcher(string directoryPath)
+        {
+            directoryChangeWatcher = new FileSystemWatcher(directoryPath, "*.*")
+            {
+                NotifyFilter = NotifyFilters.LastWrite,
+                IncludeSubdirectories = true,
+                InternalBufferSize = 16384,
+            };
+
+            directoryChangeWatcher.Changed += new FileSystemEventHandler(OnChanged_EventArrived);
+            directoryChangeWatcher.EnableRaisingEvents = true;
+        }
+
+        private void OnChanged_EventArrived(object sender, FileSystemEventArgs e)
+        {
+            OnFileChange?.Invoke(new Types.FileInfo
+            {
+                Path = e.FullPath,
+                Name = e.Name,
+                LastWrite = DateTime.Now
+            });
+        }
+
+        public bool Stop()
+        {
+            try
+            {
+                directoryChangeWatcher.EnableRaisingEvents = false;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.LogToFile(ExceptionType.Service);
+                return false;
+            }
+        }
+    }
+}
---
+++ ClientService/Watchers/Types/FileInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BeSafe.Watchers.Types
+{
+    public class FileInfo
+    {
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public DateTime LastWrite { get; set; }
+    }
+}
