0c34d2cd3a239c97d0521e47914e8d0e01c7a265
MojtabaTajik <onyfel@gmail.com>
2018-05-05 18:31:08 +0430

    Add PathUtils.cs
    Add Utils directory
    Add PathUtils.cs class which help to get common path uses by service
    Add "ApplicationName" resource string that return "BeSafe" constant

---
+++ ClientService/Utils/PathUtils.cs
@@ -0,0 +1,23 @@
+﻿using BeSafe.Properties;
+using System;
+using System.IO;
+
+namespace BeSafe.Utils
+{
+    public class PathUtils
+    {
+        public static string BeSafeSecureVolumePath
+        {
+            get
+            {
+                string myDocumentspath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string beSafeSecureVolumePath = Path.Combine(myDocumentspath, Resources.ApplicationName);
+
+                if (!Directory.Exists(beSafeSecureVolumePath))
+                    Directory.CreateDirectory(beSafeSecureVolumePath);
+
+                return beSafeSecureVolumePath;
+            }
+        }
+    }
+}
