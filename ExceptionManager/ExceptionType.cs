54c3abcc9c3d5e453d23756a61d225a275d7e407
MojtabaTajik <onyfel@gmail.com>
2018-05-06 07:47:02 +0430

    Update ExceptionTypes
    Update ExceptionTypes by list of modules in project

--- ExceptionManager/ExceptionType.cs
+++ ExceptionManager/ExceptionType.cs
@@ -2,10 +2,17 @@
 {
     public enum ExceptionType
     {
-        UI,
-        Network,
-        File,
-        Service,
-        Unknown,
+        Unknown = 0,
+        Service = 1,
+        UI = 2,
+        Network = 3,
+        ProcessWatcher = 4,
+        ModuleWatcher = 5,
+        YaraEngine = 6,
+        SecureVolume = 7,
+        StaticEngine = 8,
+        DynamicEngine = 9,
+        VTEngine = 10,
+
     }
 }
