f5fa8f34bdcf165b4ad12e1da127a9c5f9931118
MojtabaTajik <onyfel@gmail.com>
2018-05-06 07:59:58 +0430

    Update service commands
    0 meanse Stop
    1 Meanse Start
    
    Start & Stop codes are composed of ExceptionType code of each module & diable/enable state of them.
    for example ProcessWatcher have value 4 in ExceptionTypes library so :
    Start code = 41
    Stop code = 40

---
+++ Common/ServiceCommands.cs
@@ -0,0 +1,29 @@
+﻿namespace Common
+{
+    public enum ServiceCommands
+    {
+        Stop = 0,
+        Start = 1,
+
+        StopProcessWatcher = 40,
+        StartProcessWatcher = 41,
+
+        StopModuleWatcher = 50,
+        StartModuleWatcher = 51,
+
+        StopYaraEngine = 60,
+        StartYaraEngine = 61,
+
+        StopSecureVolume = 70,
+        StartSecureVolume = 71,
+
+        StopStaticEngine = 80,
+        StartStaticEngine = 81,
+
+        StopDynamicEngine = 90,
+        StartDynamicEngine = 91,
+
+        StopVTEngine = 100,
+        StartVTEngine = 101,
+    }
+}
