2141d79d09d0111b5d2dbde419651980c1fda949
Felony <onyfel@gmail.com>
2018-05-07 09:07:24 +0430

    Add Win32APIDefinitions
    This class store all Win32 API definitions

---
+++ ClientService/Utils/Win32APIDefinitions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+
+namespace BeSafe.Utils
+{
+    public class Win32APIDefinitions
+    {
+        [DllImport("kernel32", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetFileTime(SafeFileHandle hFile, ref long lpCreationTime, ref long lpLastAccessTime, ref long lpLastWriteTime);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int QueryDosDevice(string deviceName, StringBuilder buffer, int bufSize);
+    }
+}
