d86b19d9567bd78f7ae95dd7f50077f73b4d6be8
Felony <onyfel@gmail.com>
2018-05-10 14:37:34 +0430

    Implement UI code to push components configuration to service

--- ClientUI/Setting.cs
+++ ClientUI/Setting.cs
@@ -1,12 +1,48 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
+using ClientUI.Core;
+using Common.PipeCommandStructure;
+using NamedPipeWrapper;
 
 namespace ClientUI
 {
     public partial class Setting : Form
     {
+        private readonly PipeClient pipeClient = new PipeClient();
+
         public Setting()
         {
             InitializeComponent();
+
+            pipeClient.ServerMessageEventHandler += OnServerCommandReceived;
+            pipeClient.Start();
+        }
+
+        private void OnServerCommandReceived(NamedPipeConnection<BeSafePipeCommand, BeSafePipeCommand> connection, BeSafePipeCommand command)
+        {
+            MessageBox.Show(command.Notification);
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            ComponentsEnableState enableState = new ComponentsEnableState
+            {
+                DynamicScanEngine = chbDynamicEngine.Checked,
+                ModuleWatcher = chbmoduleWatcher.Checked,
+                ProcessWatcher = chbProcessWatcher.Checked,
+                SecureVolume = chbSecureVolume.Checked,
+                StaticScanEngine = chbStaticEngine.Checked,
+                VirusTotalEngine = chbVirusTotalEngine.Checked,
+                YaraEngine = chbYaraEngine.Checked,
+                ProtectorDetectorEngine = chbProtectorDetector.Checked,
+            };
+
+            bool sendCommandResult = pipeClient.SendCommandToService(new BeSafePipeCommand
+            {
+                CommandId = Guid.NewGuid(),
+                Command = PipeCommands.ComponentConfiguration,
+                ComponentsState = enableState,
+            });
         }
     }
 }
