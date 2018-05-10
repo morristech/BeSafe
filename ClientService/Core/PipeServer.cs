4c583b473188f594697cceac4d08dcdd5f8f61fe
Felony <onyfel@gmail.com>
2018-05-10 06:59:17 +0430

    Add PipeServer.cs
    PipeServer implement a pipe server that accpet client connection and let two way communication between client and server

---
+++ ClientService/Core/PipeServer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NamedPipeWrapper;
+using ExceptionManager;
+using Common.PipeCommandStructure;
+using BeSafe.Properties;
+
+namespace BeSafe.Core
+{
+    public class PipeServer
+    {
+        public event ConnectionMessageEventHandler<BeSafePipeCommand, BeSafePipeCommand> ClientMessageEventHandler;
+
+        private NamedPipeServer<BeSafePipeCommand> server = new NamedPipeServer<BeSafePipeCommand>(Resources.ApplicationName);
+
+        public PipeServer()
+        {
+            server.Error += OnError;
+            server.ClientConnected += OnClientConnected;
+            server.ClientMessage += ClientMessageEventHandler;
+
+            server.Start();
+        }
+
+        private void OnClientConnected(NamedPipeConnection<BeSafePipeCommand, BeSafePipeCommand> connection)
+        {
+            // Just accept first connection
+            if (connection.Id != 1)
+                connection.Close();
+        }
+
+        private void OnError(Exception exception)
+        {
+            exception.Log();
+        }
+
+        public bool SendCommandToUI(BeSafePipeCommand command)
+        {
+            try
+            {
+                server.PushMessage(command);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.Log();
+            }
+
+            return false;
+        }
+    }
+}
