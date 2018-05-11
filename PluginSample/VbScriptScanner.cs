972f107e4ea488835fa7fb9847b0870981278a2a
Felony <onyfel@gmail.com>
2018-05-11 23:03:20 +0430

    Add PluginSample project
    This project is very simple plugin for BeSafe secure volume that show how to use PluginSDK
    
    This plugin return threat for script files like vbs or ps1

---
+++ PluginSample/VbScriptScanner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using PluginSDK;
+
+namespace PluginSample
+{
+    public class VbScriptScanner : IBeSafePlugin
+    {
+        public PluginInfo GetPluginInfo()
+        {
+            return new PluginInfo
+            {
+                Name = "Script Scanner",
+                Version = new Version(1, 0),
+                SupportedFileTypes = new List<string>()
+                {
+                    ".vb",
+                    ".vbe",
+                    ".vbs",
+                    ".js",
+                    ".ps1",
+                    ".psd1",
+                    ".psm1",
+                    ".bat",
+                }
+            };
+        }
+
+        public PluginResult ScanFile(dynamic parameters)
+        {
+            return new PluginResult
+            {
+                ScannedObject = (string)parameters,
+                Threat = true,
+                Rate = 100,
+                Message = "Be careful, Most of the times VBScript files are dangerous !",
+                ExtraInfo = "https://en.wikipedia.org/wiki/VBScript"
+            };
+        }
+    }
+}
