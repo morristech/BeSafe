b3418064c61d1b993a4700153d4a7126eef1bead
Felony <onyfel@gmail.com>
2018-05-11 10:41:16 +0430

    Add Start method
    - We need to control enabled state of ModuleWatcher, because of this i seprate start from constuctor which cause to watcher automatically start work when new instance of ModuleWatcher create
    - Rename refactoring

--- ClientService/Watchers/ModuleWatcher.cs
+++ ClientService/Watchers/ModuleWatcher.cs
@@ -9,30 +9,27 @@ namespace BeSafe.Watchers
     public class ModuleWatcher
     {
         public delegate void LoadModuleEventHandler(ModuleInfo moduleInfo);
-        public LoadModuleEventHandler OnLoadModule;
+        public LoadModuleEventHandler LoadModule;
 
         readonly ManagementEventWatcher _moduleLoadEvent = new ManagementEventWatcher(Resources.ModuleWatcherQuery);
 
         public ModuleWatcher()
         {
+            _moduleLoadEvent.EventArrived += moduleLoadEvent_EventArrived;
+        }
+
+        public bool Start()
+        {
             try
             {
-                _moduleLoadEvent.EventArrived += moduleLoadEvent_EventArrived;
                 _moduleLoadEvent.Start();
+                return true;
             }
             catch (Exception ex)
             {
                 ex.Log(ExceptionType.Service);
+                return false;
             }
-        }
-
-        private void moduleLoadEvent_EventArrived(object sender, EventArrivedEventArgs e)
-        {
-            OnLoadModule?.Invoke(new ModuleInfo
-            {
-                ProcessId = Convert.ToUInt32(e.NewEvent.Properties[Resources.ProcessIDField].Value),
-                ModulePath = e.NewEvent.Properties[Resources.FileNameField].Value.ToString()
-            });
         }
 
         public bool Stop()
@@ -47,6 +44,15 @@ namespace BeSafe.Watchers
                 ex.Log(ExceptionType.Service);
                 return false;
             }
+        }
+
+        private void moduleLoadEvent_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            LoadModule?.Invoke(new ModuleInfo
+            {
+                ProcessId = Convert.ToUInt32(e.NewEvent.Properties[Resources.ProcessIDField].Value),
+                ModulePath = e.NewEvent.Properties[Resources.FileNameField].Value.ToString()
+            });
         }
     }
 }
