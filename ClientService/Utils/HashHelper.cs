90f5d3e387560174f0c45130b6e100dd3d881d52
Felony <onyfel@gmail.com>
2018-05-11 17:44:20 +0430

    Add HashHelper
    Contain hash algorithm implementations

---
+++ ClientService/Utils/HashHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace BeSafe.Utils
+{
+    public static class HashHelper
+    {
+        public static string GetSHA256(byte[] buffer)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(buffer);
+                return ByteArrayToString(hashBytes);
+            }
+        }
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+
+            return hex.ToString();
+        }
+    }
+}
