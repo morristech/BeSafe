9749c81211259fb9ba90176bb68499eea49b5e42
Felony <onyfel@gmail.com>
2018-05-11 16:49:34 +0430

    Add VirusTotalEngine
    Implement a VirusTotalEngine which help us to scan file or URL using VirusTotal API

---
+++ ClientService/Scanners/VirusTotalEngine.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using VirusTotalNET.Results;
+
+namespace BeSafe.Scanners
+{
+    public class VirusTotalEngine
+    {
+        private readonly string _apiKey;
+        private VirusTotalNET.VirusTotal virusTotal;
+
+        public VirusTotalEngine(string apiKey)
+        {
+            _apiKey = apiKey;
+            virusTotal = new VirusTotalNET.VirusTotal(apiKey) { UseTLS = true };
+        }
+
+        public List<FileReport> ScanFile(List<string> Objecthash)
+        {
+            return virusTotal.GetFileReportsAsync(Objecthash).Result.ToList();
+        }
+
+        public List<UrlReport> ScanURL(List<string> urlList)
+        {
+            return virusTotal.GetUrlReportsAsync(urlList).Result.ToList();
+        }
+    }
+}
