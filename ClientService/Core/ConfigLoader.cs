d89df8f70f445a0e545ad21e830a6337e62a2b9b
Felony <onyfel@gmail.com>
2018-05-12 15:18:32 +0430

    Add PluginRegulator & ConfigLoader
    - Using this class BeSafe secure volume can check requested files with plugins and detect file is threat or no
    
    - ConfigLoader class load the current configuration from config file

---
+++ ClientService/Core/ConfigLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using ConfigManager;
+
+namespace BeSafe.Core
+{
+    public class ConfigLoader
+    {
+        #region Singleton
+        private static ConfigLoader SingletonInstance;
+        public static ConfigLoader Instance() => (SingletonInstance ?? (SingletonInstance = new ConfigLoader()));
+        #endregion    
+
+        public BeSafeConfig LoadConfig()
+        {
+            string settingFilePath = new ServiceSetting().RetriveConfigFilePath();
+
+            if (!File.Exists(settingFilePath))
+                return null;
+
+            BeSafeConfig config = new BeSafeConfig(settingFilePath);
+            config.Load();
+
+            return config;
+        }
+    }
+}
---
+++ ClientService/Core/Regulators/PluginRegulators/PluginRegulator.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Common.Utils;
+using PluginSDK;
+
+namespace BeSafe.Core.Regulators.PluginRegulators
+{
+    public class PluginRegulator
+    {
+        private readonly PluginUtils pluginUtils = new PluginUtils();
+        private readonly string _pluginsDirectoryPath;
+
+        private List<IBeSafePlugin> _plugins;
+
+        public PluginRegulator(string pluginsDirectoryPath)
+        {
+            _pluginsDirectoryPath = pluginsDirectoryPath;
+
+            _plugins = pluginUtils.GetPluginsInfo(pluginsDirectoryPath);
+        }
+
+        public PluginResult IsFileSafeToExecute(string filePath)
+        {
+            PluginResult scanResult = new PluginResult();
+
+            foreach(IBeSafePlugin plugin in _plugins)
+            {
+                string fileExt = Path.GetExtension(filePath);
+                List<string> pluginSupportedFileTypes = plugin.GetPluginInfo().SupportedFileTypes;
+
+                if (pluginSupportedFileTypes.Contains(fileExt))
+                {
+                    scanResult = plugin.ScanFile(filePath);
+
+                    if (scanResult.Threat == true)
+                        break;
+                }
+            }
+
+            return scanResult;
+        }
+    }
+}
