5be123c0d54204cea56055eeea57f7009d6c1ee0
Felony <onyfel@gmail.com>
2018-05-12 09:28:14 +0430

    Add PathUtils to Common
    - PathUtils need in diffrent projects, so i move it here
    
    - Remove PathUtils from other project

--- Common/PathUtils.cs
+++ Common/PathUtils.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.IO;
-using ClientUI.Properties;
+using Common.Properties;
 
-namespace ClientUI.Core.Utils
+namespace Common
 {
     public class PathUtils
     {
+        public static string BeSafeSecureVolumePath
+        {
+            get
+            {
+                string myDocumentspath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string beSafeSecureVolumePath = Path.Combine(myDocumentspath, Resources.ApplicationName);
+
+                if (!Directory.Exists(beSafeSecureVolumePath))
+                    Directory.CreateDirectory(beSafeSecureVolumePath);
+
+                return beSafeSecureVolumePath;
+            }
+        }
+
         public static string PluginsPath
         {
             get
