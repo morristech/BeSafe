b79f6c84fffcae504447159e105e10f051ee13cf
Felony <onyfel@gmail.com>
2018-05-14 09:05:20 +0430

    Add ServiceUtils
    Service utils contain small and common routines
    
    - GetBeSafeDriveLetter() which return BeSafe mapped drive letter
    
    - SystemHiddenDirectory() which change directory attributes to Systeam and Hidden

---
+++ ClientService/Core/Utils/ServiceUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using BeSafe.Properties;
+using ExceptionManager;
+
+namespace BeSafe.Core.Utils
+{
+    public class ServiceUtils
+    {
+        public static DriveInfo GetBeSafeDriveLetter()
+        {
+            return DriveInfo.GetDrives().FirstOrDefault(d => d.DriveFormat == Resources.SecureVolumeFileSystem);
+        }
+
+        public static bool SystemHiddenDirectory(string direcotry)
+        {
+            try
+            {
+                DirectoryInfo secureVolumeMappedPathInfo = new DirectoryInfo(direcotry);
+                secureVolumeMappedPathInfo.Attributes = FileAttributes.Directory | FileAttributes.Hidden | FileAttributes.System;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.Log();
+                return false;
+            }
+        }
+    }
+}
