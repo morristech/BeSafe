9944a1a6cc21910697a706a415a7616d55879a14
Felony <onyfel@gmail.com>
2018-05-15 16:54:10 +0430

    Remove BeSafeQuarantineFileExt
    We don't need it anymore
    Confronting with the threats handle in plugins not core

--- ClientService/Properties/Resources.Designer.cs
+++ ClientService/Properties/Resources.Designer.cs
@@ -70,15 +70,6 @@ namespace BeSafe.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to .bsq.
-        /// </summary>
-        internal static string BeSafeQuarantineFileExt {
-            get {
-                return ResourceManager.GetString("BeSafeQuarantineFileExt", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to ConfigFilePath.
         /// </summary>
         internal static string ConfigFilePathRegistryValueName {
