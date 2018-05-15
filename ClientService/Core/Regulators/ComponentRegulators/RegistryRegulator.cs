9f10c0d03488188a361b618d143207347cce8731
Felony <onyfel@gmail.com>
2018-05-15 14:45:23 +0430

    Add some common dangerous registry keys for monitor
    These aren't all dangerous keys, i will complete theme step by step

--- ClientService/Core/Regulators/ComponentRegulators/RegistryRegulator.cs
+++ ClientService/Core/Regulators/ComponentRegulators/RegistryRegulator.cs
@@ -23,9 +23,31 @@ namespace BeSafe.Core.Regulators.ComponentRegulators
 
             if ((config?.ComponentsState.RegistryWatcher == true) && (stoppingService == false))
             {
+                string userSID = config.UserSID;
+
                 registryWatcher = new RegistryWatcher(new List<RegistryMonitorPath>
                 {
-                    new RegistryMonitorPath()
+                    // CurrentUser keys
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.Users, RegistryKeyPath = $@"{userSID}\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.Users, RegistryKeyPath = $@"{userSID}\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.Users, RegistryKeyPath = $@"{userSID}\Software\Microsoft\Windows\CurrentVersion\RunServices"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.Users, RegistryKeyPath = $@"{userSID}\Software\Microsoft\Windows\CurrentVersion\Run"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.Users, RegistryKeyPath = $@"{userSID}\Software\Microsoft\Windows\CurrentVersion\RunOnce"},
+
+                    // LocalMachine keys
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.LocalMachine, RegistryKeyPath = @"Software\Microsoft\Windows\CurrentVersion\explorer\Shell Folders"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.LocalMachine, RegistryKeyPath = @"Software\Microsoft\Windows\CurrentVersion\explorer\User Shell Folders"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.LocalMachine, RegistryKeyPath = @"Software\Microsoft\Windows\CurrentVersion\RunServices"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.LocalMachine, RegistryKeyPath = @"Software\Microsoft\Windows\CurrentVersion\RunServicesOnce"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.LocalMachine, RegistryKeyPath = @"Software\Microsoft\Windows\CurrentVersion\Run"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.LocalMachine, RegistryKeyPath = @"Software\Microsoft\Windows\CurrentVersion\RunOnce"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.LocalMachine, RegistryKeyPath = @"SOFTWARE\Classes\cplfile\shell\cplopen\command"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.LocalMachine, RegistryKeyPath = @"SOFTWARE\Classes\batfile\shell\open\command"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.LocalMachine, RegistryKeyPath = @"Software\CLASSES\comfile\shell\open\command"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.LocalMachine, RegistryKeyPath = @"Software\CLASSES\exefile\shell\open\command"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.LocalMachine, RegistryKeyPath = @"Software\CLASSES\htafile\Shell\Open\Command"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.LocalMachine, RegistryKeyPath = @"Software\CLASSES\piffile\shell\open\command"},
+                    new RegistryMonitorPath{RegistryHive = RegistryHive.LocalMachine, RegistryKeyPath = @"SOFTWARE\Classes\scrfile\shell\open\command"},
                 });
 
                 registryWatcher.ValueChanged += ValueChangedArrived;
