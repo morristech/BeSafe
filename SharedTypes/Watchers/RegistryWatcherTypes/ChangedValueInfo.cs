e53cf5f1302dc7320b29b293854f5bd13bfcb835
Felony <onyfel@gmail.com>
2018-05-15 17:53:20 +0430

    Add SharedTypes project
    This project contain types that should share with other projects,
    
    The main porpose of creating this project is to store types that is common in BeSafe and 3rd party plugins

---
+++ SharedTypes/Watchers/RegistryWatcherTypes/ChangedValueInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SharedTypes.Watchers.RegistryWatcherTypes
+{
+    public class ChangedValueInfo
+    {
+        public RegistryMonitorPath MonitorPath{get;set;}
+
+        public KeyValuePair<string, string> KeyValue { get; set; }
+
+        public new string ToString()
+        {
+            return $@"{MonitorPath.RegistryHive}\{MonitorPath.RegistryKeyPath} - {KeyValue.Key} = {KeyValue.Value}";
+        }
+    }
+}
---
+++ SharedTypes/Watchers/RegistryWatcherTypes/RegistryMonitorPath.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Win32;
+
+namespace SharedTypes.Watchers.RegistryWatcherTypes
+{
+    public class RegistryMonitorPath
+    {
+        public RegistryHive RegistryHive { get; set; }
+
+        public string RegistryKeyPath { get; set; }
+    }
+}
