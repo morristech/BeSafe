44635d88750ce474f4a5cd2866c000f7943540d2
Felony <onyfel@gmail.com>
2018-05-16 08:29:53 +0430

    Add ToString() method which return simple string of scan result

--- PluginSDK/PluginResult.cs
+++ PluginSDK/PluginResult.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharedTypes.Watchers.RegistryWatcherTypes;
+using System;
 
 namespace PluginSDK
 {
@@ -10,5 +11,28 @@ namespace PluginSDK
         public ThreatRiskRates RiskRate;
         public string Message;
         public dynamic ExtraInfo;
+
+        public new string ToString()
+        {
+            string scannedObjectString = string.Empty;
+
+            switch (PluginInfo.Type)
+            {
+                case PluginType.File:
+                    scannedObjectString = (string)ScannedObject;
+                    break;
+
+                case PluginType.Registry:
+                    {
+                        ChangedValueInfo so = (ChangedValueInfo)ScannedObject;
+                        scannedObjectString = $@"{so.MonitorPath.RegistryHive}\{so.MonitorPath}\{so.KeyValue.Key}";
+                    }
+                    break;
+            }
+
+            return $"{PluginInfo.Name}{Environment.NewLine}" +
+                     $"{Message}{Environment.NewLine}" +
+                    $"{scannedObjectString}";
+        }
     }
 }
