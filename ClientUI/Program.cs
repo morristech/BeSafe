5bb83373071d15f3be21b0d5ecd5868ad658b0db
Felony <onyfel@gmail.com>
2018-05-17 06:59:12 +0430

    Add Mutext to check only one instance of ClientUI application run in same time

--- ClientUI/Program.cs
+++ ClientUI/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ClientUI
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        private static Mutex singleInstanceAppMutex = new Mutex(true, "5a5fbcc6-a300-4089-9e3b-26c913425d76");
+
         [STAThread]
         static void Main()
         {
+            if (!singleInstanceAppMutex.WaitOne(TimeSpan.Zero, true))
+                return;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Setting());
