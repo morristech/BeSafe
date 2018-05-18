e7713480aba835fa2a277b52570a4010333cb551
Felony <onyfel@gmail.com>
2018-05-18 10:48:57 +0430

    Add ThreatLog which store logs in queue and save theme in physical file

---
+++ ClientService/Core/Utils/ThreatLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using PluginSDK;
+using ExceptionManager;
+
+namespace BeSafe.Core.Utils
+{
+    internal class ThreatLog
+    {
+        #region Singleton
+        private static ThreatLog SingletonInstance;
+        public static ThreatLog Instance() => (SingletonInstance ?? (SingletonInstance = new ThreatLog()));
+        #endregion
+
+    }
+}
