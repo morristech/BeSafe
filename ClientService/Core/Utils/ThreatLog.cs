9e4ed39acec05f8afba9c6f03458569baa18f634
Felony <onyfel@gmail.com>
2018-05-18 10:49:33 +0430

    Add() method which add an log item to queue

--- ClientService/Core/Utils/ThreatLog.cs
+++ ClientService/Core/Utils/ThreatLog.cs
@@ -13,5 +13,22 @@ namespace BeSafe.Core.Utils
         public static ThreatLog Instance() => (SingletonInstance ?? (SingletonInstance = new ThreatLog()));
         #endregion
 
+
+        private ConcurrentQueue<Dictionary<PluginType, PluginResult>> _logs;
+
+        public bool Add(Dictionary<PluginType, PluginResult> log)
+        {
+            try
+            {
+                _logs.Enqueue(log);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.Log();
+                return false;
+            }
+        }
     }
 }
