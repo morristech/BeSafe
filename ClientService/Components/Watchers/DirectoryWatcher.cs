76a44a3885c5d0b7aac75a0db001ba032c4a2c28
Felony <onyfel@gmail.com>
2018-06-01 11:48:42 +0430

    Implement DirectoryWatcher
    This watcher get list of directory path to monitor for any write occured and alert this change by raising an event

--- ClientService/Components/Watchers/DirectoryWatcher.cs
+++ ClientService/Components/Watchers/DirectoryWatcher.cs
@@ -1,6 +1,73 @@
-﻿namespace BeSafe.Components.Watchers
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using ExceptionManager;
+
+namespace BeSafe.Components.Watchers
 {
     public class DirectoryWatcher
     {
+        public FileSystemEventHandler DirectoryChanged;
+
+        readonly List<string> _mustMonitorDirectories;
+        private readonly CancellationTokenSource _cancelToken = new CancellationTokenSource();
+        private readonly List<FileSystemWatcher> _watchers = new List<FileSystemWatcher>();
+
+        public DirectoryWatcher(List<string> mustMonitorDirectories)
+        {
+            _mustMonitorDirectories = mustMonitorDirectories;
+        }
+
+        public bool Start()
+        {
+            try
+            {
+                if (DirectoryChanged == null)
+                    return false;
+
+                foreach (var watchPath in _mustMonitorDirectories)
+                {
+                    Task.Run(() =>
+                    {
+                        FileSystemWatcher watcher = new FileSystemWatcher
+                        {
+                            Path = watchPath,
+                            NotifyFilter = NotifyFilters.LastWrite,
+                            Filter = "*.*"
+                        };
+                        watcher.Changed += DirectoryChanged;
+                        watcher.EnableRaisingEvents = true;
+
+                        _watchers.Add(watcher);
+
+                    }, _cancelToken.Token);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.Log(ExceptionType.Service);
+                return false;
+            }
+        }
+
+        public bool Stop()
+        {
+            try
+            {
+                foreach (FileSystemWatcher watcher in _watchers)
+                    watcher.EnableRaisingEvents = false;
+
+                _cancelToken.Cancel();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.Log(ExceptionType.Service);
+                return false;
+            }
+        }
     }
 }
