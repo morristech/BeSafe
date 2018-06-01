7738ce2dad8d06fa0f57744379885881548e6cf3
Felony <onyfel@gmail.com>
2018-06-01 11:49:59 +0430

    Add DirectoryRegulator

---
+++ ClientService/Core/Regulators/ComponentRegulators/DirectoryRegulator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BeSafe.Core.Utils;
+using ConfigManager;
+
+namespace BeSafe.Core.Regulators.ComponentRegulators
+{
+    public class DirectoryRegulator : IComponentRegulator
+    {
+        public void Config(BeSafeConfig config, PipeServer pipeServer, bool stoppingService)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
