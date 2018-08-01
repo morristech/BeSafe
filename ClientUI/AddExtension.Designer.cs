4b107b2a812e3aae337a62af7ca2de843538d241
Felony <onyfel@gmail.com>
2018-08-01 16:41:17 +0430

    Add "AddExtension" form
    This form let user add new extension to "File Protection" list

---
+++ ClientUI/AddExtension.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ClientUI
+{
+    partial class AddExtension
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monoFlat_ThemeContainer1 = new ClientUI.MonoFlat_ThemeContainer();
+            this.btnCancel = new ClientUI.MonoFlat_Button();
+            this.tbExtension = new ClientUI.MonoFlat_TextBox();
+            this.btnAdd = new ClientUI.MonoFlat_Button();
+            this.monoFlat_ThemeContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // monoFlat_ThemeContainer1
+            // 
+            this.monoFlat_ThemeContainer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(41)))), ((int)(((byte)(50)))));
+            this.monoFlat_ThemeContainer1.Controls.Add(this.btnCancel);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.tbExtension);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.btnAdd);
+            this.monoFlat_ThemeContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.monoFlat_ThemeContainer1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.monoFlat_ThemeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.monoFlat_ThemeContainer1.Name = "monoFlat_ThemeContainer1";
+            this.monoFlat_ThemeContainer1.Padding = new System.Windows.Forms.Padding(10, 70, 10, 9);
+            this.monoFlat_ThemeContainer1.RoundCorners = true;
+            this.monoFlat_ThemeContainer1.Sizable = true;
+            this.monoFlat_ThemeContainer1.Size = new System.Drawing.Size(301, 170);
+            this.monoFlat_ThemeContainer1.SmartBounds = true;
+            this.monoFlat_ThemeContainer1.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.monoFlat_ThemeContainer1.TabIndex = 0;
+            this.monoFlat_ThemeContainer1.Text = "Add extension";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnCancel.Image = null;
+            this.btnCancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancel.Location = new System.Drawing.Point(165, 117);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(59, 41);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // tbExtension
+            // 
+            this.tbExtension.BackColor = System.Drawing.Color.Transparent;
+            this.tbExtension.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.tbExtension.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(183)))), ((int)(((byte)(191)))));
+            this.tbExtension.Image = null;
+            this.tbExtension.Location = new System.Drawing.Point(6, 68);
+            this.tbExtension.MaxLength = 32767;
+            this.tbExtension.Multiline = false;
+            this.tbExtension.Name = "tbExtension";
+            this.tbExtension.ReadOnly = false;
+            this.tbExtension.Size = new System.Drawing.Size(283, 41);
+            this.tbExtension.TabIndex = 1;
+            this.tbExtension.TextAlignment = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbExtension.UseSystemPasswordChar = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Transparent;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnAdd.Image = null;
+            this.btnAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdd.Location = new System.Drawing.Point(230, 117);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(59, 41);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // InputBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 170);
+            this.Controls.Add(this.monoFlat_ThemeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "InputBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add extension";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.monoFlat_ThemeContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MonoFlat_ThemeContainer monoFlat_ThemeContainer1;
+        private MonoFlat_TextBox tbExtension;
+        private MonoFlat_Button btnAdd;
+        private MonoFlat_Button btnCancel;
+    }
+}
---
+++ ClientUI/AddExtension.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Forms;
+
+namespace ClientUI
+{
+    public partial class AddExtension : Form
+    {
+        private AddExtension()
+        {
+            InitializeComponent();
+        }
+
+        public static string Execute()
+        {
+            using (var inputBox = new AddExtension())
+            {
+                if (inputBox.ShowDialog() == DialogResult.OK)
+                    return inputBox.tbExtension.Text;
+
+                return null;
+            }
+        }
+
+        private void btnCancel_Click(object sender, System.EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        private void btnAdd_Click(object sender, System.EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
